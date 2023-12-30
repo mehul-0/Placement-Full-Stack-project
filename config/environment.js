8c8f215c43f138bf0d738fa9c5133f6c260e63ca
mehul-0 <116076306+mehul-0@users.noreply.github.com>
2023-12-30 17:18:57 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ const developement = {
     passport_path : './config/passport-local-strategy',
     customMware_path : './config/flashMessage',
     assets_path : './assets',
-    secret_key :'#placementCell@careercamp$$Team&&&interface##Amit^^Singh%%',
+    secret_key :'#placementCell@careercamp$$Mehul',
     mongoose_path :'mongodb+srv://placement:1UbhMXi13RXuCwSn@cluster0.p5gbb.mongodb.net/placementcell?retryWrites=true&w=majority',
     api_path :'https://linkedin-jobs-scraper-api.p.rapidapi.com/jobs'
 }
